f41b24ecab3799d3fd526c4312aa3dd0f4e544b8
yassirelkhaili <elkhailiyassir@gmail.com>
2023-03-01 22:49:44 +0100

    added simple php api and pdo file

--- backend/config/pdo.php
+++ backend/config/pdo.php
@@ -1,3 +1,14 @@
 <?php 
 //database connection
-echo "Hello Backend"; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=phpcrud", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
--- backend/controllers/DBConnect.php
+++ backend/controllers/DBConnect.php
@@ -1,3 +1,16 @@
 <?php 
 //API DB interactions
-echo "Hello Backend"; 
+require "../config/pdo.php"; 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+echo "Hello Backend";
+
+$data_raw = file_get_contents("php://input");
+$data = json_decode(($data_raw)); 
+
+if ($data) {
+    $sql="INSERT INTO users(fname) VALUES(:fname)"; 
+    $stmt = $conn->prepare($sql);    
+    echo (($stmt->execute([":fname"=>$data->name])) ? "data inserted successfuly" : "error");
+}
+
