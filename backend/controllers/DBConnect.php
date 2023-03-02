4ad6d9d8b4407e8926510aacceeb26efc2a9a6d4
yassirelkhaili <elkhailiyassir@gmail.com>
2023-03-02 23:51:59 +0100

    added cr in crud

--- backend/controllers/DBConnect.php
+++ backend/controllers/DBConnect.php
@@ -3,14 +3,24 @@
 require "../config/pdo.php"; 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-echo "Hello Backend";
+header("Content-Type: application/json"); 
 
-$data_raw = file_get_contents("php://input");
-$data = json_decode(($data_raw)); 
+$method = $_SERVER["REQUEST_METHOD"]; 
 
-if ($data) {
-    $sql="INSERT INTO users(fname) VALUES(:fname)"; 
-    $stmt = $conn->prepare($sql);    
-    echo (($stmt->execute([":fname"=>$data->name])) ? "data inserted successfuly" : "error");
+switch ($method) {
+    case "POST": 
+    $data_json = file_get_contents("php://input");
+    $data = json_decode(($data_raw)); 
+    if ($data) {
+        $sql="INSERT INTO users(fname) VALUES(:fname)"; 
+        $stmt = $conn->prepare($sql);    
+        echo (($stmt->execute([":fname"=>$data->name])) ? "data inserted successfuly" : "error");
+    }
+    case "GET": 
+        $sql="SELECT * from users"; 
+        $stmt = $conn->prepare($sql); 
+        $stmt->execute(); 
+        $data = $stmt->fetchAll((PDO::FETCH_OBJ)); 
+        $data_json = json_encode($data_raw); 
+        echo $data; 
 }
-
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect,useRef} from 'react'
 import axios from "axios"
 
 const App = () => {
     const [name, setname] = useState()
+    const [users, setusers] = useState([])
+    const loading = useRef(true)
     const handleChange = (e) => {
         setname(prev=>({...prev, [e.target.name]: e.target.value}))
     }
@@ -15,7 +17,18 @@ const App = () => {
           console.log(err)
         }
     }
-    //comment
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost/backend/controllers/DBConnect.php")
+        setusers(response.data) 
+        loading.current = false
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    useEffect(() => {
+      fetchUsers();
+    }, []);
   return (
     <>
     <form onSubmit={handleSubmit}>
@@ -23,6 +36,14 @@ const App = () => {
         <input id='name' name='name' onChange={handleChange}></input>
         <button type='submit'>Send</button>
     </form>
+    <div>{loading.current ? <div>Loading...</div> : users.map((user) => {
+      const {id,fname,lname,password} = user
+      return (
+        <div key={id}>
+        {fname + lname + password}
+      </div>
+      )
+    })}</div>
     </>
   )
 }
